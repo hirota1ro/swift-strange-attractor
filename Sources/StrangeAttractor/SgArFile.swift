b15232369e23b05499e3ff03ae5f0cbc4c897383
Ichiro HIROTA <ichiro.hirota@gmail.com>
2023-02-08 14:36:50 +0900

    change interface: reading from json file

--- Sources/StrangeAttractor/SgArFile.swift
+++ Sources/StrangeAttractor/SgArFile.swift
@@ -28,9 +28,9 @@ extension SgArFile {
         guard let factory = SgArFactories.obtain(name: "\(name)") else {
             throw SgArError.noFactory(name)
         }
-        let param = SgArParam(dict: dict)
         do {
-            return try factory.create(param: param)
+            let param = try SgArParam.build(args: factory.args, dict: dict)
+            return factory.create(param: param)
         } catch SgArError.noParam(let key) {
             throw SgArError.missing(name, key)
         }
@@ -53,128 +53,265 @@ enum SgArError: Error {
     case missing(String, String)
 }
 
+enum SgArParamType {
+    case integer
+    case float
+    case string
+}
+
+struct SgArArg {
+    let name: String
+    let type: SgArParamType
+}
+extension SgArArg {
+    static func i(_ name: String) -> SgArArg { return SgArArg(name: name, type: .integer) }
+    static func f(_ name: String) -> SgArArg { return SgArArg(name: name, type: .float) }
+    static func s(_ name: String) -> SgArArg { return SgArArg(name: name, type: .string) }
+}
+
 protocol SgArFactory {
-    func create(param: SgArParam) throws -> SgArFormula
+    var args: [SgArArg] { get }
+    func create(param: SgArParam) -> SgArFormula
 }
 
 struct SgArParam {
     let dict: [String: Any]
-
-    func flt(_ key: String) throws -> CGFloat {
+}
+extension SgArParam {
+    static func build(args: [SgArArg], dict: [String: Any]) throws -> SgArParam {
+        var d: [String: Any] = [:]
+        for arg in args {
+            let name = arg.name
+            var val: Any? = nil
+            switch arg.type {
+            case .integer:
+                val = SgArParam.asInt(dict, name)
+            case .float:
+                val = SgArParam.asFloat(dict, name)
+            case .string:
+                val = SgArParam.asStr(dict, name)
+            }
+            if let v = val {
+                d[name] = v
+            } else {
+                throw SgArError.noParam(name)
+            }
+        }
+        return SgArParam(dict: d)
+    }
+    static func asInt(_ dict: [String: Any], _ key: String) -> Int? {
+        if let val = dict[key] {
+            if let ival = Int("\(val)") {
+                return ival
+            }
+        }
+        return nil
+    }
+    static func asFloat(_ dict: [String: Any], _ key: String) -> CGFloat? {
         if let val = dict[key] {
             if let fval = Float("\(val)") {
                 return CGFloat(fval)
             }
         }
-        throw SgArError.noParam(key)
+        return nil
     }
-    func str(_ key: String) throws -> String {
+    static func asStr(_ dict: [String: Any], _ key: String) -> String? {
         if let val = dict[key] {
             return "\(val)"
         }
-        throw SgArError.noParam(key)
+        return nil
     }
+}
+extension SgArParam {
+    func int(_ key: String) -> Int { return dict[key] as! Int }
+    func flt(_ key: String) -> CGFloat { return dict[key] as! CGFloat }
+    func str(_ key: String) -> String { return dict[key] as! String }
 }
 
 class SgArFactories {
     struct Bedhead: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Bedhead(a:try param.flt("a"), b:try param.flt("b"))
+        var args: [SgArArg] { return [.f("a"), .f("b")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            return SgAr.Bedhead(a: a, b: b)
         }
     }
     struct Clifford: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Clifford(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.Clifford(a: a, b: b, c: c, d: d)
         }
     }
     struct FractalDream: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.FractalDream(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.FractalDream(a: a, b: b, c: c, d: d)
         }
     }
     struct GumowskiMira: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.GumowskiMira(f:Int(try param.flt("f")), g:Int(try param.flt("g")), a:try param.flt("a"), s:try param.flt("s"), m:try param.flt("m"))
+        var args: [SgArArg] { return [.i("f"), .i("g"), .f("a"), .f("s"), .f("m")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let f = param.int("f")
+            let g = param.int("g")
+            let a = param.flt("a")
+            let s = param.flt("s")
+            let m = param.flt("m")
+            return SgAr.GumowskiMira(f:f, g:g, a:a, s:s, m:m)
         }
     }
     struct Hopalong: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Hopalong(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            return SgAr.Hopalong(a:a, b:b, c:c)
         }
     }
     struct JasonRampe1: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.JasonRampe1(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.JasonRampe1(a:a, b:b, c:c, d:d)
         }
     }
     struct JasonRampe2: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.JasonRampe2(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.JasonRampe2(a:a, b:b, c:c, d:d)
         }
     }
     struct JasonRampe3: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.JasonRampe3(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.JasonRampe3(a:a, b:b, c:c, d:d)
         }
     }
     struct JohnnySvensson: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.JohnnySvensson(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.JohnnySvensson(a:a, b:b, c:c, d:d)
         }
     }
     struct PeterDeJong: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.PeterDeJong(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.PeterDeJong(a:a, b:b, c:c, d:d)
         }
     }
     struct SymmetricIcon: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.SymmetricIcon(l:try param.flt("l"), a:try param.flt("a"), b:try param.flt("b"), g:try param.flt("g"), o:try param.flt("o"), d:Int(try param.flt("d")))
+        var args: [SgArArg] { return [.f("l"), .f("a"), .f("b"), .f("g"), .f("o"), .i("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let l = param.flt("l")
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let g = param.flt("g")
+            let o = param.flt("o")
+            let d = param.int("d")
+            return SgAr.SymmetricIcon(l:l, a:a, b:b, g:g, o:o, d:d)
         }
     }
     struct Tinkerbell: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Tinkerbell(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"), x0:try param.flt("x0"), y0:try param.flt("y0"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d"), .f("x0"), .f("y0")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            let x0 = param.flt("x0")
+            let y0 = param.flt("y0")
+            return SgAr.Tinkerbell(a:a, b:b, c:c, d:d, x0:x0, y0:y0)
         }
     }
     struct QuadraticStrange: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.QuadraticStrange(try param.str("a"))
+        var args: [SgArArg] { return [.s("a")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.str("a")
+            return SgAr.QuadraticStrange(a)
         }
     }
     struct CubicStrange: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.CubicStrange(try param.str("a"))
+        var args: [SgArArg] { return [.s("a")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.str("a")
+            return SgAr.CubicStrange(a)
         }
     }
     struct QuarticStrange: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.QuarticStrange(try param.str("a"))
+        var args: [SgArArg] { return [.s("a")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.str("a")
+            return SgAr.QuarticStrange(a)
         }
     }
     struct ChossatGolubitsky: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.ChossatGolubitsky(a:try param.flt("a"), b:try param.flt("b"), c:try param.flt("c"), d:try param.flt("d"))
+        var args: [SgArArg] { return [.f("a"), .f("b"), .f("c"), .f("d")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            let c = param.flt("c")
+            let d = param.flt("d")
+            return SgAr.ChossatGolubitsky(a:a, b:b, c:c, d:d)
         }
     }
     struct Lorenz: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Lorenz(a:try param.flt("a"), b:try param.flt("b"))
+        var args: [SgArArg] { return [.f("a"), .f("b")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            return SgAr.Lorenz(a:a, b:b)
         }
     }
     struct Cathala: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.Cathala(a:try param.flt("a"), b:try param.flt("b"))
+        var args: [SgArArg] { return [.f("a"), .f("b")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            return SgAr.Cathala(a:a, b:b)
         }
     }
     struct SprottElhadj: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.SprottElhadj(a:try param.flt("a"), b:try param.flt("b"))
+        var args: [SgArArg] { return [.f("a"), .f("b")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            return SgAr.SprottElhadj(a:a, b:b)
         }
     }
     struct ModifiedLozi: SgArFactory {
-        func create(param: SgArParam) throws -> SgArFormula {
-            return SgAr.ModifiedLozi(a:try param.flt("a"), b:try param.flt("b"))
+        var args: [SgArArg] { return [.f("a"), .f("b")] }
+        func create(param: SgArParam) -> SgArFormula {
+            let a = param.flt("a")
+            let b = param.flt("b")
+            return SgAr.ModifiedLozi(a:a, b:b)
         }
     }
 }
