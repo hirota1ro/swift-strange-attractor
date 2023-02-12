7682facc7819ca64ec83c6baa765044300c3ed9f
Ichiro HIROTA <ichiro.hirota@gmail.com>
2023-02-12 13:33:46 +0900

    move color resolver from util to image subcommand

--- Sources/StrangeAttractor/SgArImage.swift
+++ Sources/StrangeAttractor/SgArImage.swift
@@ -83,3 +83,33 @@ extension StrangeAttractor.Image {
         print("succeeded to write \(fileURL.path)")
     }
 }
+
+struct VelAccResolver {
+    let v: Span
+    let a: Span
+    let b: SgArBrightnessResolver
+}
+extension VelAccResolver: SgArColorResolver {
+    func resolve(point: CGPoint, velocity: CGPoint, acceleration: CGPoint) -> NSColor {
+        let vel = velocity.norm
+        let acc = acceleration.norm
+        let hue = v.normalized(vel)
+        let sat = easeOutQuad(a.normalized(acc))
+        let bri = b.resolve(hue: hue, saturation: sat)
+        return NSColor(hue: hue, saturation: sat, brightness: bri, alpha: 1)
+    }
+
+    func easeOutQuad(_ x: CGFloat) -> CGFloat {
+	return 1 - (1 - x) * (1 - x)
+    }
+}
+
+protocol SgArBrightnessResolver {
+    func resolve(hue: CGFloat, saturation sat: CGFloat) -> CGFloat
+}
+struct WhiteBackBrightnessResolver: SgArBrightnessResolver {
+    func resolve(hue: CGFloat, saturation sat: CGFloat) -> CGFloat { return sat }
+}
+struct BlackBackBrightnessResolver: SgArBrightnessResolver {
+    func resolve(hue: CGFloat, saturation sat: CGFloat) -> CGFloat { return 1 }
+}
