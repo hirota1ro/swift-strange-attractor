756900508c4390a71bacfca4dab72d7b3f70ee41
Ichiro HIROTA <ichiro.hirota@gmail.com>
2023-02-14 15:07:59 +0900

    add ModifiedMira map

--- Sources/StrangeAttractor/SgArFactory.swift
+++ Sources/StrangeAttractor/SgArFactory.swift
@@ -634,6 +634,21 @@ struct MacMillan: SgArFactory {
     var start: CGPoint { return CGPoint(x: 0.1, y: 0.1) }
 }
 
+struct ModifiedMira: SgArFactory {
+    var args: [SgArArgType] { return [.f("a", -1...1), .f("b", -1...1)] }
+    func next(param: SgArParam) -> SgArNext {
+        let a = param.flt("a")
+        let b = param.flt("b")
+        let f = { (_ x: CGFloat) -> CGFloat in return a*x - (3 - a)/(a + exp(b*x)) }
+        return { (_ x: CGFloat, _ y: CGFloat) -> CGPoint in
+            let xnew = b*y + f(x)
+            let ynew = -x + f(xnew)
+            return CGPoint(x: xnew, y: ynew)
+        }
+    }
+    var start: CGPoint { return CGPoint(x: -1.0, y: 8.0) }
+}
+
 class SgArFactories {
     let fmap: [String: SgArFactory]
 
@@ -688,5 +703,6 @@ class SgArFactories {
       MaynardSmith(),
       Gingerbread(),
       MacMillan(),
+      ModifiedMira(),
     ]
 }
